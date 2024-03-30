fa64af0c338fc66264ffd406d051d1a5dacaad21
WebpageCoder53 <133002197+WebpageCoder53@users.noreply.github.com>
2024-03-30 17:42:58 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 song1 = "";
 song2 = "";
+song1Status = "";
+song2Status = "";
 
 leftWristX = 0;
 rightWristX = 0;
 leftWristY = 0;
 rightWristY = 0;
+leftWristConfidence = 0;
+rightWristConfidence = 0;
 
 function preload() {
 song1 = loadSound("normal music.mp3");
@@ -24,6 +28,28 @@ function setup() {
 
 function draw() {
     image(video, 0, 0, 600, 500);
+    fill("red");
+    stroke("red");
+    song1Status = song1.isPlaying();
+    song2Status = song2.isPlaying();
+    if(leftWristConfidence > 0.2) {
+        circle(leftWristX, leftWristY, 20);
+        song2.stop();
+        if(song1Status == false) {
+        song1.play();
+        document.getElementById("song").innerHTML = "Song Name: Rick Astley - Never Gonna Give You Up"
+        }
+    }
+    if(rightWristConfidence > 0.2) {
+        circle(rightWristX, rightWristY, 20);
+        song1.stop();
+        if(song2Status == false) {
+        song2.play();
+        document.getElementById("song").innerHTML = "Song Name: Henry Stickmin - Distraction Dance"
+        }
+    }
+    
+    
 }
 
 function modelLoaded() {
@@ -35,9 +61,11 @@ function gotPoses(results) {
         console.log(results);
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
-        console.log("left wrist is at X: " + leftWristX + ", Y: " + leftWristY);
+        leftWristConfidence = results[0].pose.keypoints[9].score;
+        console.log("left wrist is at X: " + leftWristX + ", Y: " + leftWristY) + ", confidence: " + leftWristConfidence;
         rightWristX = results[0].pose.rightWrist.x;
         rightWristY = results[0].pose.rightWrist.y;
-        console.log("right wrist is at X: " + rightWristX + ", Y: " + rightWristY);
+        rightWristConfidence = results[0].pose.keypoints[10].score;
+        console.log("right wrist is at X: " + rightWristX + ", Y: " + rightWristY) + ", confidence: " + rightWristConfidence;
     }
 }
